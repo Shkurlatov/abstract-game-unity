0dc45c09f433e845add1fdbafbb3ef5f100c2a17
Alexander Shkurlatov <shkurlatov.work@gmail.com>
2024-07-04 22:05:18 +1200

    Create Board container for cards.

--- Assets/Project/Code/App/Services/Assets/AssetPath.cs
+++ Assets/Project/Code/App/Services/Assets/AssetPath.cs
@@ -3,6 +3,7 @@
     public static class AssetPath
     {
         public const string CARDS = "Cards";
+        public const string BOARD = "Game/Board";
         public const string MENU_PANEL = "UI/MenuPanel";
         public const string HOME_BUTTON = "UI/HomeButton";
         public const string SCORE_COUNTER = "UI/ScoreCounter";
